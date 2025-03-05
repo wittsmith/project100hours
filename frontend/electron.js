b8f7403c465fddd835adc26372c9d0d87af79ad2
Gregory Witt Smith <sdx3xe@virginia.edu>
2025-03-05 16:52:14 -0500

    praying

--- frontend/electron.js
+++ frontend/electron.js
@@ -6,12 +6,17 @@ function createWindow() {
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"), // Ensure preload.js is loaded
-      nodeIntegration: false
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      contextIsolation: false,  // Allow JavaScript execution
     }
   });
 
-  win.loadURL("http://localhost:3000");
+  win.loadURL("http://localhost:3000").catch((err) => {
+    console.error("Failed to load React:", err);
+  });
+
+  win.webContents.openDevTools(); // Open DevTools automatically
 }
 
 app.whenReady().then(createWindow);
